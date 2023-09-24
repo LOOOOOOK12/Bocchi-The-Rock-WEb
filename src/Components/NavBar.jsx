f75aa1434d92320937117063ba80973540973e5d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-24 13:27:44 +0800

    update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Logo from '../Images/Kessoku_Band_Logo.png'
+
 
 function NavBAr() {
   return (
-    <div>NavBAr</div>
+    <div>
+      <div className='LOGO'>
+          <img src={Logo} alt="" />
+      </div>
+        <ul className='Links'>
+          <a href="#Home"><li>Home</li></a>
+          <a href="#Synopsis"><li>Synopsis</li></a>
+          <a href="#Characters"><li>Characters</li></a>
+          <a href="#Songs"><li>Songs</li></a>
+        </ul> 
+    </div>
   )
 }
 
