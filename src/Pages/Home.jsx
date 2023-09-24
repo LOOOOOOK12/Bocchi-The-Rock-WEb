754dd82f654c23aa01a672387a13122f32d56b5d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-24 14:58:23 +0800

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div style={{height: `100vh`, 
+    <div id="Home" style={{height: `100vh`, 
                 display: `flex`,
                 alignItems:"center",
                 justifyContent:"center"}}> 
