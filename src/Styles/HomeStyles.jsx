c9b630daff1140f8a0e48c14996e0995a08e9248
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-25 19:00:02 +0800

    add

---
+++ src/Styles/HomeStyles.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HomeStyles = styled.div`
+    height: 100vh; 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+`
+
+export default HomeStyles
