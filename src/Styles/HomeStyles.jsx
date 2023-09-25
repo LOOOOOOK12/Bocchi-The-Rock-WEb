df8dac0adcfafd2467788cf88435f0004fbe9647
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-25 20:27:52 +0800

    update

--- src/Styles/HomeStyles.jsx
+++ src/Styles/HomeStyles.jsx
@@ -3,9 +3,12 @@ import styled from 'styled-components'
 
 const HomeStyles = styled.div`
     height: 100vh; 
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0,0,0,0.5)),url("src/assets/wallpaperflare.com_wallpaper (1).jpg");
+    background-size: cover;
     display: flex;
     align-items: center;
     justify-content: center;
+
     
 `
 
