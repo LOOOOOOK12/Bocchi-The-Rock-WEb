dcea35a611e4bf0a76dfb59567e017e8100c15b1
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-25 20:27:47 +0800

    update

---
+++ src/Styles/CharactersStyles.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CharactersStyles = styled.div`
+    height: 100vh; 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+
+
+export default CharactersStyles
