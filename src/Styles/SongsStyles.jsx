4c6019d67c73934628a40e3101bd316b7dd90da0
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-25 20:27:56 +0800

    update

---
+++ src/Styles/SongsStyles.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const SongsStyles = styled.div`
+    height: 100vh; 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export default SongsStyles
