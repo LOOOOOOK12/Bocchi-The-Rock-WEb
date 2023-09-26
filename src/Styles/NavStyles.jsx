a5af864d11abf1966b40190f14cb5dfa20ad18ab
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-26 12:38:41 +0800

    nav BG

--- src/Styles/NavStyles.jsx
+++ src/Styles/NavStyles.jsx
@@ -1,36 +1,40 @@
-import React from 'react'
-import styled from 'styled-components'
+    import React from 'react'
+    import styled from 'styled-components'
 
-const NavStyles = styled.div`
-        height: 28px;
-        top: 0;
-        z-index: 15;
-        width: 90%;
-        position: fixed;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        text-align: center;
-        padding: 1.5rem 5%;
-    
-    img{
-        height: 3rem;
-    }
+    const NavStyles = styled.div`
+            height: 28px;
+            top: 0;
+            z-index: 15;
+            width: 90%;
+            position: fixed;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            text-align: center;
+            padding: 1.5rem 5%;
+            transition: 0.5s;
+        
+        &.active {
+            background: #2f2f2f;
+        }
+        img{
+            height: 3rem;
+        }
 
-    li,a {
-        color: white;
-        font-weight: bold;
-        font-size: 1rem;
-        text-decoration: none;
-    }
+        li,a {
+            color: white;
+            font-weight: bold;
+            font-size: 1rem;
+            text-decoration: none;
+        }
 
-    ul li{
-        cursor: pointer;
-        list-style: none;
-        display: inline-block;
-        padding-left: 30px;
-    }
+        ul li{
+            cursor: pointer;
+            list-style: none;
+            display: inline-block;
+            padding-left: 30px;
+        }
 
-`
+    `
 
-export default NavStyles
+    export default NavStyles
