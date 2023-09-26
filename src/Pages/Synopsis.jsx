0b80d4f6a6ba7046f4f15057e8b33c51ca9cfc2b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-26 17:30:12 +0800

    update

--- src/Pages/Synopsis.jsx
+++ src/Pages/Synopsis.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import SynopsisStyles from '../Styles/SynopsisStyles'
+import poster from '../assets/wallpaperflare.com_wallpaper.jpg'
 
 function Synopsis() {
   return (
     <SynopsisStyles id="Synopsis">
-        <h1>Synopsis</h1>
+      <div className="Text-1">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis dignissimos aliquid quisquam soluta ratione possimus perspiciatis sapiente </p>
+      </div>
+        <div className='posters-Carousel'>
+          <img src={poster} alt="poster" className='Poster-1'/>
+        </div>
+        <div className='Text-2'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis dignissimos aliquid quisquam soluta ratione possimus perspiciatis sapiente 
+        </div>
     </SynopsisStyles>
   )
 }
