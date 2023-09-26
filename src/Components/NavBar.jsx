5d155f0bbe6674dea8de22a81a04da15f44c7628
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-26 12:38:36 +0800

    nav BG

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,29 @@
 import Logo from '../Images/Kessoku_Band_Logo.png'
 import NavStyles from '../Styles/NavStyles'
+import { useState, useEffect } from 'react'
 
 function NavBAr() {
 
+  const [isNavActive, setNavActive] = useState(false);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 500) {
+        setNavActive(true);
+      } else {
+        setNavActive(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <NavStyles>
+    <NavStyles className={isNavActive ? 'active' : ''}>
       <div className='LOGO'>
           <a href="#Home"><img src={Logo} alt='LOGO'></img></a>
       </div>
