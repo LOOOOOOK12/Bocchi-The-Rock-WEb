1207a3edcc4155e1e47b0c85120f0714c2bb54bb
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-27 17:29:04 +0800

    update

--- src/Pages/Characters.jsx
+++ src/Pages/Characters.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import pic from "../assets/wallpaperflare.com_wallpaper.jpg"
 import CharactersStyles from '../Styles/CharactersStyles'
 
 function Characters() {
   return (
     <CharactersStyles id='Characters'>
-      <h1>Characters</h1>
-        <div className=''></div>
-        <div className=''></div>
-        <div className=''></div>
-        <div className=''></div>
+      <h1>CAHRACTERS</h1>
     </CharactersStyles>
   )
 }
