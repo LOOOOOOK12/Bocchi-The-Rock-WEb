fbffed12574ac3202ad012f5434b409372d90f09
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-27 17:29:09 +0800

    update

--- src/Pages/Characters.jsx
+++ src/Pages/Characters.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import pic from "../assets/wallpaperflare.com_wallpaper.jpg"
 import CharactersStyles from '../Styles/CharactersStyles'
 
 function Characters() {
