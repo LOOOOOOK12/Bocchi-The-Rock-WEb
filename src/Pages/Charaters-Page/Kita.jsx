70d62676234c9dccabc58a8f04820a2d657c7d3f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-27 22:22:10 +0800

    update

---
+++ src/Pages/Charaters-Page/Kita.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import KitaStyles from '../../Styles/Characters-Styles/KitaStyles'
+
+function Kita() {
+  return (
+    <KitaStyles>
+        <img src="" alt="" />
+        <div>
+        <h1>Hitori Gotoh</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </KitaStyles>
+  )
+}
+
+export default Kita
