1452cf93f18792454d7bd0081ce0a3b66c0ede8f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-27 22:21:41 +0800

    update

--- src/Pages/Characters.jsx
+++ src/Pages/Characters.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import CharactersStyles from '../Styles/CharactersStyles'
+import Bocchi from './Charaters-Page/Bocchi';
+import Kita from './Charaters-Page/Kita';
+import Ryo from './Charaters-Page/Ryo';
+import Nijika from './Charaters-Page/Nijika';
+
 
 function Characters() {
   return (
     <CharactersStyles id='Characters'>
-      <h1>CAHRACTERS</h1>
+      <Swiper
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide className='Bocchi'><Bocchi/></SwiperSlide>
+        <SwiperSlide className='Kita'><Kita/></SwiperSlide>
+        <SwiperSlide className='Ryo'><Ryo/></SwiperSlide>
+        <SwiperSlide className='Nijika'><Nijika/></SwiperSlide>
+      </Swiper>
     </CharactersStyles>
   )
 }
