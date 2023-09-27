88d74475827d0e0a3a2b3a274cd70c208ce80d65
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-27 17:29:14 +0800

    update

--- src/Pages/Synopsis.jsx
+++ src/Pages/Synopsis.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import pic from "../assets/wallpaperflare.com_wallpaper.jpg"
 import SynopsisStyles from '../Styles/SynopsisStyles'
 import poster from '../assets/wallpaperflare.com_wallpaper.jpg'
 
@@ -8,9 +11,17 @@ function Synopsis() {
       <div className="Text-1">
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis dignissimos aliquid quisquam soluta ratione possimus perspiciatis sapiente </p>
       </div>
-        <div className='posters-Carousel'>
-          <img src={poster} alt="poster" className='Poster-1'/>
-        </div>
+          <Swiper
+            spaceBetween={0}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide><img src={pic} alt="" /></SwiperSlide>
+            <SwiperSlide><img src={pic} alt="" /></SwiperSlide>
+            <SwiperSlide><img src={pic} alt="" /></SwiperSlide>
+            <SwiperSlide><img src={pic} alt="" /></SwiperSlide>
+          </Swiper>
         <div className='Text-2'>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis dignissimos aliquid quisquam soluta ratione possimus perspiciatis sapiente 
         </div>
