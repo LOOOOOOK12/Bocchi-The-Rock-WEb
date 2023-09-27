a858c4bd5a01bc5c6e5f0ac7ad6a61cfc2bdfc03
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-27 22:22:44 +0800

    update

---
+++ src/Pages/Charaters-Page/Ryo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import RyoStyles from '../../Styles/Characters-Styles/RyoStyles'
+
+function Ryo() {
+  return (
+    <RyoStyles>
+        <img src="" alt="" />
+        <div>
+        <h1>Hitori Gotoh</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </RyoStyles>
+  )
+}
+
+export default Ryo
