890b158e725f6b159fa1944711bc6da96b6d79a1
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-27 22:22:38 +0800

    update

---
+++ src/Pages/Charaters-Page/Nijika.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NijikaStyles from '../../Styles/Characters-Styles/NijikaStyles'
+
+function Nijika() {
+  return (
+    <NijikaStyles>
+        <img src="" alt="" />
+        <div>
+        <h1>Hitori Gotoh</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </NijikaStyles>
+  )
+}
+
+export default Nijika
