0b47f625eaf9e2388d2c4892bcd47c55030dbdcd
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-28 17:45:54 +0800

    update

--- src/Pages/Charaters-Page/Ryo.jsx
+++ src/Pages/Charaters-Page/Ryo.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import RyoPicture from "../../assets/Ryo_Yamada_Character_Design_2.png"
 import RyoStyles from '../../Styles/Characters-Styles/RyoStyles'
 
 function Ryo() {
   return (
     <RyoStyles>
-        <img src="" alt="" />
+        <div className='Profile'>
+          <img src={RyoPicture} alt="" />
+        </div>
         <div>
             <h1>Ryo Yamada</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
