68605c81be20dd12e092fa5e5e85e3cd9b152b10
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-28 12:39:12 +0800

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import HomeStyles from '../Styles/HomeStyles'
 import homeLogo from '../assets/Kessoku_Band_Logo.png'
+import Logo2 from '../assets/bocchi-the-rock-logo-00522AF4F8-seeklogo.com.png'
 
 function Home() {
 
   return (
     <HomeStyles id= "Home"> 
-        <img src={homeLogo} alt="LOGO" />
+        <img src={Logo2} alt="LOGO" />
     </HomeStyles>
   )
 }
