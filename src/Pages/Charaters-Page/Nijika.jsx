0b9edd048cc976083716a409d81910205a88cfc3
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-28 17:45:51 +0800

    update

--- src/Pages/Charaters-Page/Nijika.jsx
+++ src/Pages/Charaters-Page/Nijika.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import NijikaPicture from "../../assets/Nijika_Ijichi_Character_Design_2.png"
 import NijikaStyles from '../../Styles/Characters-Styles/NijikaStyles'
 
 function Nijika() {
   return (
     <NijikaStyles>
-        <img src="" alt="" />
+        <img src={NijikaPicture} alt="" />
         <div>
             <h1>Nijika Ichiji</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
