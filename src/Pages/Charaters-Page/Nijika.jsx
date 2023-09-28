0ce35e82e0971bb0c22a2173bbaa26cbeb2f809c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-28 12:39:31 +0800

    update

--- src/Pages/Charaters-Page/Nijika.jsx
+++ src/Pages/Charaters-Page/Nijika.jsx
@@ -6,8 +6,8 @@ function Nijika() {
     <NijikaStyles>
         <img src="" alt="" />
         <div>
-        <h1>Hitori Gotoh</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h1>Nijika Ichiji</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
     </NijikaStyles>
   )
