b98b14f2666d944ae89ffba1fbeef75bd361517d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-28 17:45:46 +0800

    update

--- src/Pages/Charaters-Page/Kita.jsx
+++ src/Pages/Charaters-Page/Kita.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import KitaPicture from "../../assets/Ikuyo_Kita_Character_Design_2.png"
 import KitaStyles from '../../Styles/Characters-Styles/KitaStyles'
 
 function Kita() {
   return (
     <KitaStyles>
-        <img src="" alt="" />
+        <img src={KitaPicture} alt="" />
         <div>
         <h1>Kita Ikuyo</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
