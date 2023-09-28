5fb969b883535eadf308f6de6afef42e6212255e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-28 12:39:19 +0800

    update

--- src/Pages/Charaters-Page/Bocchi.jsx
+++ src/Pages/Charaters-Page/Bocchi.jsx
@@ -6,8 +6,8 @@ function Bocchi() {
     <BocchiStyles>
         <img src="" alt="" />
         <div>
-        <h1>Hitori Gotoh</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h1>Hitori Gotoh</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
     </BocchiStyles>
   )
