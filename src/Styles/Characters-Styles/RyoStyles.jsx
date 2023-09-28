0d9a64434ea6d56a17430cfc6578caeba87a9f32
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-28 12:40:31 +0800

    update

--- src/Styles/Characters-Styles/RyoStyles.jsx
+++ src/Styles/Characters-Styles/RyoStyles.jsx
@@ -4,7 +4,10 @@ import styled from 'styled-components'
 const RyoStyles = styled.div`
     height: 100vh;
     background: blue;
-
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
 export default RyoStyles
