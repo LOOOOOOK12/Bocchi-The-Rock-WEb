448b16ab60d4d0ada3c01b251799cd00d01f816b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-28 17:45:40 +0800

    update

--- src/Pages/Charaters-Page/Bocchi.jsx
+++ src/Pages/Charaters-Page/Bocchi.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import BocchiPicture from "../../assets/Hitori_Gotoh_Character_Design_2.png"
 import BocchiStyles from '../../Styles/Characters-Styles/BocchiStyles'
 
 function Bocchi() {
   return (
     <BocchiStyles>
-        <img src="" alt="" />
+        <img src={BocchiPicture} alt="" />
         <div>
             <h1>Hitori Gotoh</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
