bf9671df4982a6aca536dd56b116e7dc49884837
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-29 23:19:39 +0800

    update

--- src/Styles/Characters-Styles/NijikaStyles.jsx
+++ src/Styles/Characters-Styles/NijikaStyles.jsx
@@ -3,11 +3,19 @@ import styled from 'styled-components'
 
 const NijikaStyles = styled.div`
     height: 100vh;
-    background: yellow;
+    background: #323232;
     position: relative;
     display: flex;
     justify-content: center;
     align-items: center;
+    text-align: center;
+
+    .Profile{
+        position: relative;
+        height: 100%;
+        border-radius: 52% 48% 46% 54% / 68% 48% 52% 32%    ;
+        background: yellow;
+    }
 
     img{
         height: 100%;
