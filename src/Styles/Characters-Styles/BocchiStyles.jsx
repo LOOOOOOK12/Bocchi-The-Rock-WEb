c5aa192f28aa3c9e2e4ad4efeeb01987d0a0cf58
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-29 23:19:31 +0800

    update

--- src/Styles/Characters-Styles/BocchiStyles.jsx
+++ src/Styles/Characters-Styles/BocchiStyles.jsx
@@ -3,16 +3,31 @@ import styled from 'styled-components'
 
 const BocchiStyles = styled.div`
     height: 100vh;
-    background: pink;
+    background: #323232;
     position: relative;
     display: flex;
     justify-content: center;
     align-items: center;
+    text-align: center;
+   
 
-
-    img{
-       height: 100% ;
+    .Shape{
+        position: relative;
+        height: 100%;
+        border-radius: 46% 54% 46% 54% / 68% 48% 52% 32%   ;
+        background: pink;
     }
+
+    .Text{
+        position: relative;
+    }
+
+    img{ 
+        position: relative;
+        height: 100%;
+    }
+
+    
 `
 
 export default BocchiStyles
