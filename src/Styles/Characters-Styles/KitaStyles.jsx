2e8b395c40bb51d2e72e80ffebb5db1c414eae8b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-29 23:19:35 +0800

    update

--- src/Styles/Characters-Styles/KitaStyles.jsx
+++ src/Styles/Characters-Styles/KitaStyles.jsx
@@ -3,11 +3,19 @@ import styled from 'styled-components'
 
 const KitaStyles = styled.div`
     height: 100vh;
-    background: red;
+    background: #323232;
     position: relative;
     display: flex;
     justify-content: center;
     align-items: center;
+    text-align: center;
+
+    .Profile{
+        position: relative;
+        height: 100%;
+        border-radius: 46% 54% 46% 54% / 68% 48% 52% 32%   ;
+        background: red;
+    }
 
     img{
         height: 100%;
