4d552ebaeb58607ce5c360caf12054b4cf58da98
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-30 18:43:38 +0800

    update

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FooterStyles from '../Styles/FooterStyles'
+
+function Footer() {
+  return (
+    <FooterStyles>
+        <h2>Bocchi the Rock</h2>
+    </FooterStyles>
+  )
+}
+
+export default Footer
