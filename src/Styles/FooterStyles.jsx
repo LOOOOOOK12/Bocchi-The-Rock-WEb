b3066601a99e144446d57026684369aef7381dfa
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-09-30 18:44:00 +0800

    update

---
+++ src/Styles/FooterStyles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FooterStyles = styled.div`
+    height: 12rem;
+    background-color:black ;
+    
+`
+export default FooterStyles
