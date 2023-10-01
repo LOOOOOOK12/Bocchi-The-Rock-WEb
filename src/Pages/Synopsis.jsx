952979e8053f7a491b3edf575785a946b1ad546b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-01 14:46:54 +0800

    update

--- src/Pages/Synopsis.jsx
+++ src/Pages/Synopsis.jsx
@@ -3,18 +3,16 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import pic from "../assets/wallpaperflare.com_wallpaper.jpg"
 import SynopsisStyles from '../Styles/SynopsisStyles'
-import Poster1 from "../assets/Bocchi_the_Rock 21_Anime_Main_Key_Visual.png"
+import Poster1 from "../assets/Posters/Bocchi_the_Rock 21_Anime_Main_Key_Visual.png"
+import Poster2 from "../assets/Posters/Blu-ray_ 26_DVD_Volume_5.png"
+import Poster3 from "../assets/Posters/Blu-ray_ 26_DVD_Volume_6.png"
+import Poster4 from "../assets/Posters/Bocchi_the_Rock 21_Theater_Compilation_Key_Visual.png"
+
 
 function Synopsis() {
   return (
       <SynopsisStyles id="Synopsis">
         <div className='synopsis-Content'>
-          <div className="Text-1">
-            <p>Bocchi the Rock! is a band comedy 4-koma manga series written and illustrated by Aki Hamazi. 
-              The manga series serialized in Manga Time Kirara MAX by Houbunsha since February 2018 issue and 
-              collected five volumes in tankōbon format as of November 2022, and has an official translation 
-              by Yen Press, which will debut on October 17, 2023.</p>
-          </div>
           <div className="Swiper">
             <Swiper 
                 spaceBetween={0}
@@ -23,17 +21,17 @@ function Synopsis() {
                 onSwiper={(swiper) => console.log(swiper)}
               >
                 <SwiperSlide><img src={Poster1} alt="BTR Key Visuals" /></SwiperSlide>
-                <SwiperSlide><img src={pic} alt="" /></SwiperSlide>
-                <SwiperSlide><img src={pic} alt="" /></SwiperSlide>
-                <SwiperSlide><img src={pic} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={Poster2} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={Poster3} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={Poster4} alt="" /></SwiperSlide>
             </Swiper>
           </div>
-            <div className='Text-2'>
-            In this story, Hitori Gotoh is a high school girl who's starting to learn to play the guitar because she 
-            dreams of being in a band, but she's so shy that she hasn't made a single friend. However, her dream 
-            might come true after she meets Nijika Ijichi, a girl who plays drums and is looking for a new guitarist 
-            for her band.
-            </div>
+          <div className="Text-1">
+            <p>Bocchi the Rock! is a band comedy 4-koma manga series written and illustrated by Aki Hamazi. 
+              The manga series serialized in Manga Time Kirara MAX by Houbunsha since February 2018 issue and 
+              collected five volumes in tankōbon format as of November 2022, and has an official translation 
+              by Yen Press, which will debut on October 17, 2023.</p>
+          </div>
         </div>
       </SynopsisStyles>
   )
