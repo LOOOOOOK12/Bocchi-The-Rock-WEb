f5b51adb6ffd2940c82429147238317f8b05b796
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-02 22:03:46 +0800

    update

--- src/Styles/SynopsisStyles.jsx
+++ src/Styles/SynopsisStyles.jsx
@@ -2,37 +2,34 @@ import React from 'react'
 import styled from 'styled-components'
 
 const SynopsisStyles = styled.div`
-    height: 100vh; 
+    height: 100vh;
     display: flex;
     align-items: center;
     justify-content: center;
     text-align: center;
     padding: 2rem 4rem;
     background: #222222;
-    /* background-image: url("src/assets/Site-community-image.png");
-    background-size: cover;
-    background-attachment: fixed; */
-    
+    gap: 3rem;
+
+    .Swiper{
+        height: 85%;
+        width: 25rem;
+    }
 
     img{
-        height: 30rem;
+        height: 100%;
         width: 100%;
     }
 
-    .Swiper{
-        width: 20rem;
-    }
-
     .Text-1{
-        line-height: 1.3rem;
+        width: 50%;
+        line-height: 1.5rem;
     }
 
     @media (max-width: 700px) {
-        #Synopsis{
             display: flex;
             flex-direction: column;
         }
-    }
 `
 
 export default SynopsisStyles
