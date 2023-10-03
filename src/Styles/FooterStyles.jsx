5d149692d82949515798e5aa9751e0ba51684498
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-03 07:55:34 -0400

    update

--- src/Styles/FooterStyles.jsx
+++ src/Styles/FooterStyles.jsx
@@ -3,16 +3,16 @@ import styled from 'styled-components'
 
 const FooterStyles = styled.div`
     height: 12rem;
-    background-color: #202020;
+    background-color: #ffff32;
     display: flex;
     align-items: center;
     justify-content: space-around;
+    color: #131313;
     
-    
-
     .right-Container a{
-        color: white;
+        color: #131313;
         text-decoration: none;
+        margin: 0 1.5rem;
     }
 `
 export default FooterStyles
