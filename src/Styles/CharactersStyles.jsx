a956c1627b5e27d6878f98c75aaa1f2e550732c6
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-07 09:54:14 -0400

    update

--- src/Styles/CharactersStyles.jsx
+++ src/Styles/CharactersStyles.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 
 const CharactersStyles = styled.div`
-    height: 100vh; 
     width: 100%;
     display: flex;
     align-items: center;
