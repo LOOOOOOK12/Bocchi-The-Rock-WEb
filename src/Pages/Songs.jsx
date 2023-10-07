09d6c7cb9903cd1ae9fed3a41eebf588256c0da5
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-07 09:54:10 -0400

    update

--- src/Pages/Songs.jsx
+++ src/Pages/Songs.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import SongStyles from '../Styles/SongsStyles'
+import Pic from '../assets/Posters/Blu-ray_ 26_DVD_Volume_5.png'
+import { useRef, useEffect } from 'react';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 function Songs() {
+
+  const swiperElRef = useRef(null);
+
+  useEffect(() => {
+    swiperElRef.current.addEventListener('progress', (e) => {
+      const [swiper, progress] = e.detail;
+      console.log(progress);
+    });
+
+    swiperElRef.current.addEventListener('slidechange', (e) => {
+      console.log('slide changed');
+    });
+  }, []);
+
   return (
     <SongStyles id='Songs'>
-      <h1>Songs</h1>
+      <swiper-container
+      ref={swiperElRef}
+      slides-per-view="1"
+      navigation="true"
+      pagination="true"
+      speed="500" 
+      loop="true"
+      css-mode="true" 
+      id='Container'
+      >
+        <swiper-slide className = "slides">
+          <img src={Pic} alt="" className='Pic1' />
+        </swiper-slide>
+        <swiper-slide className = "slides">
+          <img src={Pic} alt="" className='Pic1' />
+        </swiper-slide>
+        <swiper-slide className = "slides">
+          <img src={Pic} alt="" className='Pic1' />
+        </swiper-slide>
+      </swiper-container>
     </SongStyles>
   )
 }
