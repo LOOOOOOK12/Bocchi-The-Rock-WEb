6b9b09061d7feb56513c0fa27af69bf2590a64f7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-08 03:43:14 -0400

    update

--- src/Pages/Songs.jsx
+++ src/Pages/Songs.jsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules'
 import SongStyles from '../Styles/SongsStyles'
 import Pic from '../assets/Posters/Blu-ray_ 26_DVD_Volume_5.png'
+import Album1 from '../assets/Album Cover/Seishun_Complex_Single_Cover_29 (1).png'
 import 'swiper/css/autoplay'
 import 'swiper/css';
 
@@ -14,10 +15,11 @@ function Songs() {
       <Swiper
         modules={[Autoplay]}
         autoplay
+        loop
         id='Container'
       >
         <SwiperSlide className = "slides" >
-          <img src={Pic} alt="" className='Pic1'/>
+          <img src={Album1} alt="" className='Pic1'/>
         </SwiperSlide>
         <SwiperSlide className = "slides">
           <img src={Pic} alt="" className='Pic1'/>
