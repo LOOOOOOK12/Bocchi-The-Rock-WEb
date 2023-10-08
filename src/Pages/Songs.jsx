77bf0a41d6df558127a2132b71887b8e3188fdbf
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-08 03:20:21 -0400

    update

--- src/Pages/Songs.jsx
+++ src/Pages/Songs.jsx
@@ -1,47 +1,31 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules'
 import SongStyles from '../Styles/SongsStyles'
 import Pic from '../assets/Posters/Blu-ray_ 26_DVD_Volume_5.png'
-import { useRef, useEffect } from 'react';
-import { register } from 'swiper/element/bundle';
+import 'swiper/css/autoplay'
+import 'swiper/css';
 
-register();
 
 function Songs() {
 
-  const swiperElRef = useRef(null);
-
-  useEffect(() => {
-    swiperElRef.current.addEventListener('progress', (e) => {
-      const [swiper, progress] = e.detail;
-      console.log(progress);
-    });
-
-    swiperElRef.current.addEventListener('slidechange', (e) => {
-      console.log('slide changed');
-    });
-  }, []);
-
   return (
     <SongStyles id='Songs'>
-      <swiper-container
-      ref={swiperElRef}
-      slides-per-view="1"
-      navigation="true"
-      pagination="true"
-      autoplay = "true"
-      crossFade = "true"
-      id='Container'
+      <Swiper
+        modules={[Autoplay]}
+        autoplay
+        id='Container'
       >
-        <swiper-slide className = "slides" >
+        <SwiperSlide className = "slides" >
           <img src={Pic} alt="" className='Pic1'/>
-        </swiper-slide>
-        <swiper-slide className = "slides">
+        </SwiperSlide>
+        <SwiperSlide className = "slides">
           <img src={Pic} alt="" className='Pic1'/>
-        </swiper-slide>
-        <swiper-slide className = "slides">
+        </SwiperSlide>
+        <SwiperSlide className = "slides">
           <img src={Pic} alt="" className='Pic1'/>
-        </swiper-slide>
-      </swiper-container>
+        </SwiperSlide>
+      </Swiper>
     </SongStyles>
   )
 }
