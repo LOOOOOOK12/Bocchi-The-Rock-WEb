b22af5a85882c4060aefd069502248e76f7a5513
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-08 03:07:25 -0400

    update

--- src/Pages/Characters.jsx
+++ src/Pages/Characters.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import { EffectFade, Autoplay } from 'swiper/modules'
 import CharactersStyles from '../Styles/CharactersStyles'
 import Bocchi from './Charaters-Page/Bocchi';
 import Kita from './Charaters-Page/Kita';
 import Ryo from './Charaters-Page/Ryo';
 import Nijika from './Charaters-Page/Nijika';
-
-
+import 'swiper/css';
+import 'swiper/css/effect-fade'
+import 'swiper/css/autoplay'
 function Characters() {
+
   return (
     <CharactersStyles id='Characters'>
       <Swiper
-        className='Swiper'
-        slidesPerView={1}
-      >
-        <SwiperSlide className='Bocchi'><Bocchi/></SwiperSlide>
-        <SwiperSlide className='Kita'><Kita/></SwiperSlide>
-        <SwiperSlide className='Ryo'><Ryo/></SwiperSlide>
-        <SwiperSlide className='Nijika'><Nijika/></SwiperSlide>
-      </Swiper>
+        modules={[ EffectFade, Autoplay]}
+        effect={'fade'}
+        loop
+        autoplay
+        id = "Container"
+    >
+      <SwiperSlide><Bocchi/></SwiperSlide>
+      <SwiperSlide><Kita/></SwiperSlide>
+      <SwiperSlide><Ryo/></SwiperSlide>
+      <SwiperSlide><Nijika/></SwiperSlide>
+    </Swiper>
     </CharactersStyles>
   )
 }
