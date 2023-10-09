9b9f87fe0f39120728595dcccd7c9071a3da609a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-09 09:26:46 -0400

    update

--- src/Pages/Songs-Page/Song2.jsx
+++ src/Pages/Songs-Page/Song2.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Song2Styles from '../../Styles/Song-Styles/Song2Styles'
+import album2 from '../../assets/Album Cover/Ano_Band_Digital_Cover_29.png'
 
 function Song2() {
   return (
     <Song2Styles>
-      Song2
+      <img src={album2} alt="album2" />
     </Song2Styles>
   )
 }
