05698af10071c7c639d89569d3ebcce0f45fea7e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-09 09:05:48 -0400

    add

---
+++ src/Pages/Songs-Page/Song3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Song3Styles from '../../Styles/Song-Styles/Song3Styles'
+
+function Song3() {
+  return (
+    <Song3Styles>
+      Song3
+    </Song3Styles>
+  )
+}
+
+export default Song3
