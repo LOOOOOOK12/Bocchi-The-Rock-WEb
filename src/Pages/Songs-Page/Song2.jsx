9f653f568f0ee3349090d2beb17d4229815542af
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-09 09:05:44 -0400

    add

---
+++ src/Pages/Songs-Page/Song2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Song2Styles from '../../Styles/Song-Styles/Song2Styles'
+
+function Song2() {
+  return (
+    <Song2Styles>
+      Song2
+    </Song2Styles>
+  )
+}
+
+export default Song2
