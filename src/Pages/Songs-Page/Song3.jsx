823fd6a096622da674515326973379e78be17c4b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-09 09:26:50 -0400

    update

--- src/Pages/Songs-Page/Song3.jsx
+++ src/Pages/Songs-Page/Song3.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Song3Styles from '../../Styles/Song-Styles/Song3Styles'
+import album3 from '../../assets/Album Cover/Wasurete_Yaranai_Digital_Cover_29.png'
 
 function Song3() {
   return (
     <Song3Styles>
-      Song3
+      <img src={album3} alt="album3" />
     </Song3Styles>
   )
 }
