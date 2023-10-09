2cd8f5ba7278386de29e5f96e06f2f8253a3e2c7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-09 09:05:54 -0400

    add

---
+++ src/Pages/Songs-Page/Song4.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Song4Styles from '../../Styles/Song-Styles/Song4Styles'
+
+function Song4() {
+  return (
+    <Song4Styles>
+      Song4
+    </Song4Styles>
+  )
+}
+
+export default Song4
