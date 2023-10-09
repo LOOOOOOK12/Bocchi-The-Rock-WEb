adc41aff4eed3202d145dd8b57ce7aaf9a555160
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-09 09:26:54 -0400

    update

--- src/Pages/Songs-Page/Song4.jsx
+++ src/Pages/Songs-Page/Song4.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Song4Styles from '../../Styles/Song-Styles/Song4Styles'
+import album4 from '../../assets/Album Cover/Seiza_ni_Naretara_Digital_Cover_29.png'
 
 function Song4() {
   return (
     <Song4Styles>
-      Song4
+      <img src={album4} alt="album4" />
     </Song4Styles>
   )
 }
