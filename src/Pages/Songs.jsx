58e4983a476738a9ec408d1b59dac6f2a489e459
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-10 07:29:31 -0400

    update

--- src/Pages/Songs.jsx
+++ src/Pages/Songs.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules'
 import SongStyles from '../Styles/SongsStyles'
 import Song1 from './Songs-Page/Song1';
 import Song2 from './Songs-Page/Song2';
 import Song3 from './Songs-Page/Song3';
 import Song4 from './Songs-Page/Song4';
-import 'swiper/css/autoplay'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade, Autoplay } from 'swiper/modules'
 import 'swiper/css';
+import 'swiper/css/effect-fade'
+import 'swiper/css/autoplay'
 
 
 function Songs() {
@@ -15,21 +16,23 @@ function Songs() {
   return (
     <SongStyles id='Songs'>
       <Swiper
-        modules={[Autoplay]}
+        modules={[Autoplay,EffectFade]}
+        effect={'fade'}
+        crossFade = 'true'
         autoplay
         loop
         id='Container'
       >
-        <SwiperSlide className = "slides" >
+        <SwiperSlide>
           <Song1/>
         </SwiperSlide>
-        <SwiperSlide className = "slides">
+        <SwiperSlide>
           <Song2/>
         </SwiperSlide>
-        <SwiperSlide className = "slides">
+        <SwiperSlide>
           <Song3/>
         </SwiperSlide>
-        <SwiperSlide className = "slides">
+        <SwiperSlide>
           <Song4/>
         </SwiperSlide>
       </Swiper>
