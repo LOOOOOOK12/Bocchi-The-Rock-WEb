7eba88f133b6e6dcea61a64f154c4e0ec03c8ce6
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-10 07:30:02 -0400

    update

--- src/Styles/Song-Styles/Song1Styles.jsx
+++ src/Styles/Song-Styles/Song1Styles.jsx
@@ -3,6 +3,27 @@ import styled from 'styled-components'
 const Song1Styles = styled.div`
     height: 100%;
     width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: linear-gradient(rgba(0, 0, 0, 0.7),rgba(0,0,0,0.8)),url("src/assets/Album Cover/Seishun_Complex_Single_Cover_29 (1).png");
+    background-size: cover;
+    background-repeat: no-repeat;
+    
+    .Container{
+        backdrop-filter: blur(8px);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 3rem;
+        /* border: 2px solid transparent; */
+    }
+
+    img{
+        height: 20rem;
+        width: 20rem;
+    }
 `
 
 export default Song1Styles
