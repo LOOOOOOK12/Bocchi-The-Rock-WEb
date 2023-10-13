f435938b6420ca14a80ede5e0dddbf60f84fc5d6
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-13 16:50:03 +0800

    update

--- src/Pages/Synopsis.jsx
+++ src/Pages/Synopsis.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import SynopsisStyles from '../Styles/SynopsisStyles'
+// import { posters }  from '../Pages/Songs-Page/Posters'
 import Poster1 from "../assets/Posters/Bocchi_the_Rock 21_Anime_Main_Key_Visual.png"
 import Poster2 from "../assets/Posters/Blu-ray_ 26_DVD_Volume_5.png"
 import Poster3 from "../assets/Posters/Blu-ray_ 26_DVD_Volume_6.png"
