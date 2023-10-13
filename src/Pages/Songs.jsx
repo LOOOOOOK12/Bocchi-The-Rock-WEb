12ec4007e4d7bb49800bd0345c178817f2e0425e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-13 16:49:59 +0800

    update

--- src/Pages/Songs.jsx
+++ src/Pages/Songs.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import SongStyles from '../Styles/SongsStyles'
 import Song1 from './Songs-Page/Song1';
-import Song2 from './Songs-Page/Song2';
-import Song3 from './Songs-Page/Song3';
-import Song4 from './Songs-Page/Song4';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Autoplay } from 'swiper/modules'
 import 'swiper/css';
 import 'swiper/css/effect-fade'
 import 'swiper/css/autoplay'
-
+import { songs } from './Songs-Page/Songs';
 
 function Songs() {
 
@@ -23,18 +20,16 @@ function Songs() {
         loop
         id='Container'
       >
-        <SwiperSlide>
-          <Song1/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Song2/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Song3/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Song4/>
-        </SwiperSlide>
+        {songs.map((song, idx) => (
+          <SwiperSlide key={idx}>
+            <Song1 
+              name={song.SongName} 
+              cover={song.albumCover} 
+              song={song.song}
+              AlbumBgStyle={song.albumBackGround}
+            />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </SongStyles>
   )
