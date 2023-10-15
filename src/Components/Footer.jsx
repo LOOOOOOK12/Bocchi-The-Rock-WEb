24e4ac07ae4ca120fed6655a6c9cc00168fae068
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-15 15:36:19 +0800

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,6 +4,7 @@ import FooterStyles from '../Styles/FooterStyles'
 function Footer() {
   return (
     <FooterStyles>
+      <div className='Container-1'>
         <div className='left-Container'>
           <h2>Bocchi the Rock Website</h2>
         </div>
@@ -13,6 +14,10 @@ function Footer() {
           <a href="#Characters">Characters</a>
           <a href="#Songs">Songs</a>
         </div>
+      </div>
+      <div className='Container-2'>
+          <p>Made with Love by Luke Symon Alamil</p>
+      </div>
     </FooterStyles>
   )
 }
