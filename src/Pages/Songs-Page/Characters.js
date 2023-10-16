0909182327e83c74b95bb903c7c2506b0cd85368
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-16 21:36:28 +0800

    update

---
+++ src/Pages/Songs-Page/Characters.js
@@ -0,0 +1,39 @@
+import BocchiPicture from "../../assets/Characters/Hitori_Gotoh_Character_Design_2.png"
+import KitaPicture from "../../assets/Characters/Ikuyo_Kita_Character_Design_2.png"
+import NijikaPicture from "../../assets/Characters/Nijika_Ijichi_Character_Design_2.png"
+import RyoPicture from "../../assets/Characters/Ryo_Yamada_Character_Design_2.png"
+
+import BocchiStyles from "../../Styles/Characters-Styles/BocchiStyles"
+import KitaStyles from "../../Styles/Characters-Styles/KitaStyles"
+import RyoStyles from "../../Styles/Characters-Styles/RyoStyles"
+import NijikaStyles from "../../Styles/Characters-Styles/NijikaStyles"
+
+export const characterProfiles = [
+    {
+        image: BocchiPicture,
+        CharName:"Hitori Gotoh",
+        description:"lorem",
+        bg: BocchiStyles
+    },
+
+    {
+        image: KitaPicture,
+        CharName:"Kita Ikuyo",
+        description: "lorem",
+        bg: KitaStyles
+    },
+
+    {
+        image: NijikaPicture,
+        CharName:"Nijika Ichiji",
+        description:"lorem",
+        bg: NijikaStyles
+    },
+
+    {
+        image: RyoPicture,
+        CharName:"Ryo Yamada",
+        description:"lorem",
+        bg: RyoStyles
+    },
+]
