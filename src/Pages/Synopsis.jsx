7bb8084965e5ea3574fd837d02dbd64d4dfff57a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-16 20:50:00 +0800

    update

--- src/Pages/Synopsis.jsx
+++ src/Pages/Synopsis.jsx
@@ -2,26 +2,22 @@ import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import SynopsisStyles from '../Styles/SynopsisStyles'
-// import { posters }  from '../Pages/Songs-Page/Posters'
-import Poster1 from "../assets/Posters/Bocchi_the_Rock 21_Anime_Main_Key_Visual.png"
-import Poster2 from "../assets/Posters/Blu-ray_ 26_DVD_Volume_5.png"
-import Poster3 from "../assets/Posters/Blu-ray_ 26_DVD_Volume_6.png"
-import Poster4 from "../assets/Posters/Bocchi_the_Rock 21_Theater_Compilation_Key_Visual.png"
-
+import { posters }  from '../Pages/Songs-Page/Posters'
 
 function Synopsis() {
   return (
       <SynopsisStyles id="Synopsis">
         <Swiper 
           className='Swiper'
+          autoplay
           loop
           slidesPerView={1}
-          autoplay
           >
-          <SwiperSlide><img src={Poster1} alt="BTR Key Visuals-1" /></SwiperSlide>
-          <SwiperSlide><img src={Poster2} alt="BTR Key Visuals-2" /></SwiperSlide>
-          <SwiperSlide><img src={Poster3} alt="BTR Key Visuals-3" /></SwiperSlide>
-          <SwiperSlide><img src={Poster4} alt="BTR Key Visuals-4" /></SwiperSlide>
+            {posters.map((posters, idx) => (
+            <SwiperSlide key={idx}>
+              <img src={posters.poster} alt={posters.alt}/>
+            </SwiperSlide>
+          ))}  
         </Swiper>       
         <div className="Text-1">
           <p>Bocchi the Rock! is a band comedy 4-koma manga series written and illustrated by Aki Hamazi. 
