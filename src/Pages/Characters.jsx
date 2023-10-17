1a40de9c6cfd9ed6b436deffe98d31772878ab61
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-17 20:03:32 +0800

    update

--- src/Pages/Characters.jsx
+++ src/Pages/Characters.jsx
@@ -6,40 +6,39 @@ import CharactersPage from "./Charaters-Page/CharactersPage"
 import {characterProfiles} from "../Pages/Songs-Page/Characters"
 
 import CharactersStyles from '../Styles/CharactersStyles'
-import Bocchi from './Charaters-Page/Bocchi';
-import Kita from './Charaters-Page/Kita';
-import Ryo from './Charaters-Page/Ryo';
-import Nijika from './Charaters-Page/Nijika';
 import 'swiper/css';
 import 'swiper/css/effect-fade'
 import 'swiper/css/autoplay'
-function Characters() {
 
+function Characters() {
   return (
     <CharactersStyles id='Characters'>
       <Swiper
-        modules={[ EffectFade, Autoplay]}
+        modules={[EffectFade, Autoplay]}
         effect={'fade'}
         loop
         autoplay
         disableOnInteraction
         id = "Container"
-    >
-      {/* {characterProfiles.map((char, idx)=>(
-        <SwiperSlide key={idx}>
-          <CharactersPage
-            profile = {char.image}
-            name = {char.name}
-            desc = {char.description}
-            background = {char.bg}
-          />
-        </SwiperSlide>
-      ))} */}
-      <SwiperSlide><Bocchi/></SwiperSlide>
-      <SwiperSlide><Kita/></SwiperSlide>
-      <SwiperSlide><Ryo/></SwiperSlide>
-      <SwiperSlide><Nijika/></SwiperSlide>
-    </Swiper>
+      >
+
+        {characterProfiles.map((profiles, idx) => (
+          <SwiperSlide key={idx}>
+            <CharactersPage
+              CharName={profiles.CharName} 
+              image ={profiles.image} 
+              description={profiles.description}
+              bg={profiles.bg}
+            />
+          </SwiperSlide>
+        ))}
+
+        {/* <SwiperSlide><Bocchi/></SwiperSlide>
+        <SwiperSlide><Kita/></SwiperSlide>
+        <SwiperSlide><Ryo/></SwiperSlide>
+        <SwiperSlide><Nijika/></SwiperSlide> */}
+        
+      </Swiper>
     </CharactersStyles>
   )
 }
