d35bb873f50aa2909fc22ac5f6f9f038ad90072e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-20 21:46:08 +0800

    update

--- src/Styles/Song-Styles/Song2Styles.jsx
+++ src/Styles/Song-Styles/Song2Styles.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import album2 from '../../assets/Album Cover/Ano_Band_Digital_Cover_29.png'
 
 const Song2Styles = styled.div`
     height: 100%;
@@ -12,13 +11,13 @@ const Song2Styles = styled.div`
 
     .Container{
         backdrop-filter: blur(8px);
+        background: linear-gradient(rgba(0, 0, 0, 0.4),rgba(0,0,0,0.4));
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
         padding: 3rem;
         border-radius: 2rem;
-        /* border: 2px solid transparent; */
     }
 
     img{
