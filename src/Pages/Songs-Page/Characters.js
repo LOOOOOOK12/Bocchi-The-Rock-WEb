5342a6c43255ba8e86d36baa25f0c15e87eec5a9
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-20 21:45:48 +0800

    update

--- src/Pages/Songs-Page/Characters.js
+++ src/Pages/Songs-Page/Characters.js
@@ -2,7 +2,6 @@ import BocchiPicture from "../../assets/Characters/Hitori_Gotoh_Character_Design
 import KitaPicture from "../../assets/Characters/Ikuyo_Kita_Character_Design_2.png"
 import NijikaPicture from "../../assets/Characters/Nijika_Ijichi_Character_Design_2.png"
 import RyoPicture from "../../assets/Characters/Ryo_Yamada_Character_Design_2.png"
-
 import BocchiStyles from "../../Styles/Characters-Styles/BocchiStyles"
 import KitaStyles from "../../Styles/Characters-Styles/KitaStyles"
 import RyoStyles from "../../Styles/Characters-Styles/RyoStyles"
