5a06f70f7270ed1e6dd089cab948b80a27c7ee3a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-23 21:44:36 +0800

    update

---
+++ src/Constants/Posters.js
@@ -0,0 +1,28 @@
+import Poster1 from "../assets/Posters/Bocchi_the_Rock 21_Anime_Main_Key_Visual.png"
+import Poster2 from "../assets/Posters/Blu-ray_ 26_DVD_Volume_5.png"
+import Poster3 from "../assets/Posters/Blu-ray_ 26_DVD_Volume_6.png"
+import Poster4 from "../assets/Posters/Bocchi_the_Rock 21_Theater_Compilation_Key_Visual.png"
+
+    export const posters = [
+
+        {
+            poster: Poster1,
+            alt: "Bocchi_the_Rock 21_Anime_Main_Key_Visual"
+        },
+
+        {
+            poster: Poster2,
+            alt:"Blu-ray_ 26_DVD_Volume_5"
+        },
+
+        {
+            poster: Poster3,
+            alt:"Blu-ray_ 26_DVD_Volume_6"
+        },
+
+        {
+            poster: Poster4,
+            alt:"Bocchi_the_Rock 21_Theater_Compilation_Key_Visual"
+        }
+
+    ]
