67429122b9561438861e235175df3b9fb7a4c61a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-26 18:07:42 +0800

    update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,26 +6,8 @@ import Burgir from './Burgir'
 
 function NavBAr() {
 
-  const [isNavActive, setNavActive] = useState(false);
-  
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY >= 500) {
-        setNavActive(true);
-      } else {
-        setNavActive(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
-    <NavStyles className={isNavActive ? 'active' : ''}>
+    <NavStyles>
       <div className='LOGO'>
           <a href="#Home"><img src={Logo} alt='LOGO'></img></a>
       </div>
