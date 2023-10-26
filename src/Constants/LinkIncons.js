377d74efd43fd4e217a6c748027dc9096b21a379
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-10-26 18:44:43 +0800

    update

--- src/Constants/LinkIncons.js
+++ src/Constants/LinkIncons.js
@@ -1,11 +1,31 @@
-
+import Facebook from "../assets/Link Icons/icons8-facebook-50.png"
+import Github from "../assets/Link Icons/icons8-github-50.png"
+import Instagram from "../assets/Link Icons/icons8-instagram-50.png"
+import LinkedIn from "../assets/Link Icons/icons8-linkedin-50.png"
 
 export const LinkIcons = [
 
-{},
-{},
-{},
-{},
+{
+    Logo: Facebook,
+    alt: "Facebook Link",
+    link: "https://www.facebook.com/luke.alamil.1/"
+
+},
+{
+    Logo: Github,
+    alt: "Github Link",
+    link: "https://github.com/LOOOOOOK12"
+},
+{
+    Logo:Instagram,
+    alt: "Instagram Link",
+    link:""
+},
+{
+    Logo:LinkedIn,
+    alt: "LinkedIn Link",
+    link: "https://www.linkedin.com/in/luke-symon-alamil-a9b924252/"
+},
 
 
 ]
