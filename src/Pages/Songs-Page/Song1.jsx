475fe08e82f6912047c2eb0c3931be544cc6d722
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-02 11:07:23 +0800

    update

--- src/Pages/Songs-Page/Song1.jsx
+++ src/Pages/Songs-Page/Song1.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Song1({ name, cover, song, AlbumBgStyle }) {
+function Song1({ name, cover, alt, song, AlbumBgStyle }) {
   
   const StyledContainer = AlbumBgStyle;
 
   return (
     <StyledContainer>
         <div className="Container">
-          <img src={cover} alt="album1" />
+          <img src={cover} alt={alt} />
           <h2>{name}</h2>
           <audio  controls>
             <source src={song} type="audio/mpeg" />
