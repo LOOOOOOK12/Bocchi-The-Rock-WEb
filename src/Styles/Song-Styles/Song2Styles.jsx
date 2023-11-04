3ae9cd28f054a2692ac7a4be3416e35065b84291
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-04 18:27:52 +0800

    update

--- src/Styles/Song-Styles/Song2Styles.jsx
+++ src/Styles/Song-Styles/Song2Styles.jsx
@@ -20,6 +20,11 @@ const Song2Styles = styled.div`
         border-radius: 2rem;
     }
 
+    a{
+        text-decoration: none;
+        color: white;
+    }
+
     img{
         height: 20rem;
         width: 20rem;
