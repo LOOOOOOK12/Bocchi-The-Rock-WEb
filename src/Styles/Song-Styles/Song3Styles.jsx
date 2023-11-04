dbc0ca0895e50c17e3c829bd35041d5bafd6d0f8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-04 18:27:56 +0800

    update

--- src/Styles/Song-Styles/Song3Styles.jsx
+++ src/Styles/Song-Styles/Song3Styles.jsx
@@ -20,6 +20,11 @@ const Song3Styles = styled.div`
         border-radius: 2rem;
     }
 
+    a{
+        text-decoration: none;
+        color: white;
+    }
+
     img{
         height: 20rem;
         width: 20rem;
