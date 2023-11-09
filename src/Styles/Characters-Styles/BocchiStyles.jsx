68a2aceb0c23ffbdaf746a2c2337046363603a7f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-09 17:33:34 +0800

    update

--- src/Styles/Characters-Styles/BocchiStyles.jsx
+++ src/Styles/Characters-Styles/BocchiStyles.jsx
@@ -26,7 +26,7 @@ const BocchiStyles = styled.div`
         flex-direction: column;
         text-align: left;
         line-height: 1.8rem;
-        margin: 0 1.5rem;
+        margin: 0 1rem;
         width: 350px;
     }
 
@@ -45,14 +45,15 @@ const BocchiStyles = styled.div`
         font-size: 12px;
     }
 
-    
-
     .Shape{
         height: 70%;
     }
     }
 
     @media (max-width: 375px) {
+
+        gap: 0rem;
+
         .Text{
         font-size: 13px;
         line-height: 1.5rem;
