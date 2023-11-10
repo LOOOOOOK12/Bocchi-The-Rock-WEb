3ac67118f85d6930219a4718f8349ee699543a24
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-10 17:04:05 +0800

    update

--- src/Pages/Songs-Page/Song1.jsx
+++ src/Pages/Songs-Page/Song1.jsx
@@ -3,27 +3,7 @@ import React, { useRef, useState } from 'react';
 function Song1({ name, cover, alt, song, AlbumBgStyle, lyrics }) {
   const StyledContainer = AlbumBgStyle;
 
-  // Create a ref to hold the audio element
-  const audioRef = useRef(null);
-
-  // State to track which audio is currently playing
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  // Function to stop the currently playing audio
-  const stopCurrentAudio = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-      audioRef.current.currentTime = 0;
-      setIsPlaying(false);
-    }
-  };
-
-  // Function to handle the play event on the audio element
-  const handlePlay = () => {
-    // Stop the currently playing audio if another audio is played
-    stopCurrentAudio();
-    setIsPlaying(true);
-  };
+  
 
   return (
     <StyledContainer>
@@ -32,7 +12,7 @@ function Song1({ name, cover, alt, song, AlbumBgStyle, lyrics }) {
           <img src={cover} alt={alt} />
         </div>
         <a href={lyrics} target="_blank"><h2>{name}</h2></a>
-          <audio controls ref={audioRef} onPlay={handlePlay}>
+          <audio controls>
           <source src={song} type="audio/mpeg" />
         </audio>
       </div>
