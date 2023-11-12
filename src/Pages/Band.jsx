422e914853eb248a4c5b1831adead5ff28dcbaf8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-12 15:55:01 +0800

    update

--- src/Pages/Band.jsx
+++ src/Pages/Band.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import BandStyles from '../Styles/BandStyles'
+import BG from '../assets/Video/BG.mp4'
 
 function Band() {
     return (
         <BandStyles>
+            <video autoPlay loop muted playsInline className='bg-Video'>
+                <source src={BG} type="video/mp4" />
+            </video>
             <h1>Meet The Band!!</h1>
+            
         </BandStyles>
     )
 }
