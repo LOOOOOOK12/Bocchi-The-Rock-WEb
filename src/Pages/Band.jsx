129f6d171f173ba69fd37c9067e05ca6019b33e5
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-12 14:56:20 +0800

    update

---
+++ src/Pages/Band.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BandStyles from '../Styles/BandStyles'
+
+function Band() {
+    return (
+        <BandStyles>
+            <h1>Meet The Band!!</h1>
+        </BandStyles>
+    )
+}
+
+export default Band
