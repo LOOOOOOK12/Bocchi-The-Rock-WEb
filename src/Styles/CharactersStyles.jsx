c1f6fe33301ebf36e6b1d3cd3dfa4bf4efe1b1a3
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-12 14:56:26 +0800

    update

--- src/Styles/CharactersStyles.jsx
+++ src/Styles/CharactersStyles.jsx
@@ -4,6 +4,19 @@ import styled from 'styled-components'
 const CharactersStyles = styled.div`
     height: 100vh;
     background: #151515 ;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    .Band{
+        background: #d67373;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 40%;
+    }
 
     #Container{
         height: 100%;
