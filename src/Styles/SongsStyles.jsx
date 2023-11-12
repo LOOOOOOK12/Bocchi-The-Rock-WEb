9c83dbfd25df8cb72cc8379b23f6ca1ca9586a71
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-12 14:56:28 +0800

    update

--- src/Styles/SongsStyles.jsx
+++ src/Styles/SongsStyles.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const SongsStyles = styled.div`
-    height:150vh;
+    height: 120vh;
     display: flex;
     align-items: center;
     justify-content: center;
