5cdc554564299dbc0f0eb7ba6d22d7acd8640f80
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-12 14:56:35 +0800

    update

---
+++ src/Styles/Song-Styles/Song9Styles.jsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+const Song9Styles = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: linear-gradient(rgba(0, 0, 0, 0.4),rgba(0,0,0,0.4)),url("src/assets/Album Cover/Karakara_Digital_Cover_29.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    .Container{
+        backdrop-filter:blur(6px);
+        background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0,0,0,0.3));
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 2.2rem;
+        border-radius: 2.5rem;
+    }
+
+    a{
+        text-decoration: none;
+        color: #e1e1e1;
+    }
+    
+    .img-Container
+    {
+        height: 20rem;
+        width: 20rem;
+    }
+
+    img{
+        height: 100%;
+        width: 100%;
+        border-radius: 2rem;
+    }
+
+    audio{
+        width: 100%;
+    }
+
+    @media (max-width: 425px) {
+        .img-Container{
+        height: 20rem;
+        width: 17rem;
+    }
+}
+    
+`
+
+export default Song9Styles
