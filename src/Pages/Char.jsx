1c9379b125e217d1471c5c52989754e775136b60
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-13 20:03:34 +0800

    update

---
+++ src/Pages/Char.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CharStyles from '../Styles/CharStyles'
+
+function Char() {
+    return (
+        <CharStyles>
+            <h1>Song List!</h1>
+        </CharStyles>
+    )
+}
+
+export default Char
