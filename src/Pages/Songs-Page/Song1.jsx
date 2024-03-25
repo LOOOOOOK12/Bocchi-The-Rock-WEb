0752b8c70a664ae70dc3d804abbcb1444d5a8e36
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-25 19:13:49 +0800

    update

--- src/Pages/Songs-Page/Song1.jsx
+++ src/Pages/Songs-Page/Song1.jsx
@@ -1,19 +1,19 @@
 
-function Song1({ name, cover, alt, song, AlbumBgStyle, lyrics }) {
-  const StyledContainer = AlbumBgStyle;
+function Song1({ name, cover, alt, song, lyrics, albumBackGround}) {
 
   return (
-    <StyledContainer>
-      <div className="Container">
-        <div className='img-Container'>
-          <img src={cover} alt={alt} />
+    <div className="h-full w-full flex items-center justify-center relative">
+      <img src={albumBackGround} alt="" className="absolute brightness-[.4] h-full w-full object-cover -z-10"/>
+      <div className="flex flex-col items-center justify-center p-9 rounded-[2.5rem] backdrop-blur-md gap-5 ">
+        <div className='h-80 w-80'>
+          <img className="h-full w-full rounded-[2.5rem]" src={cover} alt={alt} />
         </div>
-        <a href={lyrics} target="_blank"><h2>{name}</h2></a>
-          <audio controls>
+        <a className="text-3xl font-semibold" href={lyrics} target="_blank"><h2>{name}</h2></a>
+          <audio controls className="w-full">
           <source src={song} type="audio/mpeg" />
         </audio>
       </div>
-    </StyledContainer>
+    </div>
   );
 }
 
