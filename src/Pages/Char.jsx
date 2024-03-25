ea4bb09083be70dc15cfa7646e23f86df6da5a26
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-25 19:13:43 +0800

    update

--- src/Pages/Char.jsx
+++ src/Pages/Char.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import CharStyles from '../Styles/CharStyles'
-
+import image from '../assets/bgPicture.jpg'
 function Char() {
     return (
-        <CharStyles>
-            <h1>Song List!</h1>
-        </CharStyles>
+        <div className='relative flex items-center justify-center h-[43vh]'>
+            <img src={image} alt="" className='absolute brightness-[.2] h-screen w-full object-cover -z-10'/>
+            <h1 className='text-5xl font-bold'>Song List!</h1>
+        </div>
     )
 }
 
